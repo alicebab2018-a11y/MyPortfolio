89136711fc0eb577cb3a25496363ee7813270212
Dammy <alicebab2018@gmail.com>
2026-07-21 16:31:06 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 20) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
